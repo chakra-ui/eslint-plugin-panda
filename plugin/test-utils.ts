5c1a3f37602bd2b790de2127bb6d1cbe9f85a878
anubra266 <anubra266@gmail.com>
2024-01-23 08:01:07 -0600

    Add vitest tests

--- plugin/test-utils.ts
+++ plugin/test-utils.ts
@@ -1,4 +1,11 @@
 import { RuleTester, type RuleTesterConfig } from '@typescript-eslint/rule-tester'
+import * as vitest from 'vitest'
+
+RuleTester.afterAll = vitest.afterAll
+
+RuleTester.it = vitest.it
+RuleTester.itOnly = vitest.it.only
+RuleTester.describe = vitest.describe
 
 const baseTesterConfig: RuleTesterConfig = {
   parser: '@typescript-eslint/parser',
