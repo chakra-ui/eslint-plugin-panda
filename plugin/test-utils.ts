dc83befd13efddd538a13dc6fa18c8a2d252ae97
anubra266 <anubra266@gmail.com>
2024-01-23 08:37:27 -0600

    settle deps and tests

--- plugin/test-utils.ts
+++ plugin/test-utils.ts
@@ -1,11 +1,4 @@
 import { RuleTester, type RuleTesterConfig } from '@typescript-eslint/rule-tester'
-import * as vitest from 'vitest'
-
-RuleTester.afterAll = vitest.afterAll
-
-RuleTester.it = vitest.it
-RuleTester.itOnly = vitest.it.only
-RuleTester.describe = vitest.describe
 
 const baseTesterConfig: RuleTesterConfig = {
   parser: '@typescript-eslint/parser',
---
+++ tests-setup.ts
@@ -0,0 +1,4 @@
+import { afterAll } from 'vitest'
+import { RuleTester } from '@typescript-eslint/rule-tester'
+
+RuleTester.afterAll = afterAll
