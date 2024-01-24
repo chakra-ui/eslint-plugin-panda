b1b2459966252d01eee1c759b07465ccdefd19e3
anubra266 <anubra266@gmail.com>
2024-01-23 23:16:16 -0600

    New docs

--- .eslintdocgenrc.js
+++ .eslintdocgenrc.js
@@ -2,6 +2,9 @@ module.exports = {
   docPath: 'docs/rules/{name}.md',
   rulePath: 'plugin/src/rules/{name}.ts',
   testPath: 'test/{name}.test.ts',
-  pluginName:"@pandacss"
+  pluginName: '@pandacss',
+  excludeExamplesByDefault: true,
+  showFilenames: true,
+  minExamples: null,
 }
 
